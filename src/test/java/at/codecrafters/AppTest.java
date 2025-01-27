8ffadb809e9bf9518941b8e60418625935787787
wienm <martin.scheibelreiter@chello.at>
2025-01-27 12:29:04 +0100

    (at)DisplayName

--- src/test/java/at/codecrafters/AppTest.java
+++ src/test/java/at/codecrafters/AppTest.java
@@ -1,11 +1,14 @@
 package at.codecrafters;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test Math operations in Calculator class")
 class AppTest {
     //test<System Under Test>_<Condition or State Change>_<Result>
+    @DisplayName("Test App is not null")
     @Test
     void testApp_initApp_appNotNull(){
         App app = new App();
@@ -13,6 +16,7 @@ class AppTest {
     }
 
     //test<System Under Test>_<Condition or State Change>_<Result>
+    @DisplayName("Test assertEquals")
     @Test
     void testAssertEquals_AcutalResultEqualsExpectedResult_Success(){
         int expectedResult = 4;
