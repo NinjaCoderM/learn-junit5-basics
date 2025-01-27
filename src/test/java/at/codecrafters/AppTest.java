99d76896154cf7c75c52e46b44d7ff24c350457a
wienm <martin.scheibelreiter@chello.at>
2025-01-27 12:19:34 +0100

    Naming Unit Test

--- src/test/java/at/codecrafters/AppTest.java
+++ src/test/java/at/codecrafters/AppTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    //test<System Under Test>_<Condition or State Change>_<Result>
     @Test
-    void testApp(){
+    void testApp_initApp_appNotNull(){
         App app = new App();
-        assertNotNull(app, "App is null");
+        assertNotNull(app, "Expected: App is not Null");
+    }
+
+    //test<System Under Test>_<Condition or State Change>_<Result>
+    @Test
+    void testAssertEquals_AcutalResultEqualsExpectedResult_Success(){
         int expectedResult = 4;
         int actualResult = expectedResult;
         assertEquals(expectedResult, actualResult, ()-> "Expeceted Result: " + expectedResult + ", but actual: " + actualResult + " Lazy Assert Message, Lambda only executed when test fails " );
