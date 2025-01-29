0536ad9d41fa9784b7c37e428b0b4ad68cde598a
wienm <martin.scheibelreiter@chello.at>
2025-01-29 22:42:10 +0100

    ParameterizedTest MethodSource Tip

--- src/test/java/at/codecrafters/AppTest.java
+++ src/test/java/at/codecrafters/AppTest.java
@@ -31,12 +31,12 @@ class AppTest {
 
     @DisplayName("ParameterizedTest MethodSource [expectedResult, actualResult]")
     @ParameterizedTest
-    @MethodSource("parameterizedTestInputParameters")
+    @MethodSource()
     void testParameterizedMethodSource_AcutalResultEqualsExpectedResult_Success(int expectedResult, int actualResult){
         assertEquals(expectedResult, actualResult, ()-> "Expeceted Result: " + expectedResult + ", but actual: " + actualResult + " Lazy Assert Message, Lambda only executed when test fails " );
     }
 
-    private static Stream<Arguments> parameterizedTestInputParameters(){
+    private static Stream<Arguments> testParameterizedMethodSource_AcutalResultEqualsExpectedResult_Success(){
         return Stream.of(
                 Arguments.of(10, 10),
                 Arguments.of(100, 100)
