82b22daae843192bb8079102f050b15317760f04
wienm <martin.scheibelreiter@chello.at>
2025-01-29 23:45:00 +0100

    TestMethodOrder(MethodOrderer.Random.class)

---
+++ src/test/java/at/codecrafters/MethodOrderedTest.java
@@ -0,0 +1,25 @@
+package at.codecrafters;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedTest {
+    @Test
+    void testA(){
+        System.out.println("Running testA");
+    }
+    @Test
+    void testB(){
+        System.out.println("Running testB");
+    }
+    @Test
+    void testC(){
+        System.out.println("Running testC");
+    }
+    @Test
+    void testD(){
+        System.out.println("Running testD");
+    }
+}
