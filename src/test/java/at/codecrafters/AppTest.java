2eb292368b50dcbd3758e8a89b34decff7cef1c8
wienm <martin.scheibelreiter@chello.at>
2025-01-29 22:41:07 +0100

    ParameterizedTest MethodSource

--- src/test/java/at/codecrafters/AppTest.java
+++ src/test/java/at/codecrafters/AppTest.java
@@ -2,6 +2,11 @@ package at.codecrafters;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,4 +28,19 @@ class AppTest {
         int actualResult = expectedResult;
         assertEquals(expectedResult, actualResult, ()-> "Expeceted Result: " + expectedResult + ", but actual: " + actualResult + " Lazy Assert Message, Lambda only executed when test fails " );
     }
+
+    @DisplayName("ParameterizedTest MethodSource [expectedResult, actualResult]")
+    @ParameterizedTest
+    @MethodSource("parameterizedTestInputParameters")
+    void testParameterizedMethodSource_AcutalResultEqualsExpectedResult_Success(int expectedResult, int actualResult){
+        assertEquals(expectedResult, actualResult, ()-> "Expeceted Result: " + expectedResult + ", but actual: " + actualResult + " Lazy Assert Message, Lambda only executed when test fails " );
+    }
+
+    private static Stream<Arguments> parameterizedTestInputParameters(){
+        return Stream.of(
+                Arguments.of(10, 10),
+                Arguments.of(100, 100)
+        );
+    }
+
 }
