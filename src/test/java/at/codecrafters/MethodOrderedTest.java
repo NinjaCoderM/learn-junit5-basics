6061bbaee3a5cd8dbe0e3d4299c9f5d02e6dd93d
wienm <martin.scheibelreiter@chello.at>
2025-01-30 13:50:15 +0100

    TestMethodOrder(MethodOrderer.OrderAnnotation.class)

--- src/test/java/at/codecrafters/MethodOrderedTest.java
+++ src/test/java/at/codecrafters/MethodOrderedTest.java
@@ -1,24 +1,29 @@
 package at.codecrafters;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedTest {
     @Test
+    @Order(4)
     void testA(){
         System.out.println("Running testA");
     }
     @Test
+    @Order(2)
     void testB(){
         System.out.println("Running testB");
     }
     @Test
+    @Order(3)
     void testC(){
         System.out.println("Running testC");
     }
     @Test
+    @Order(1)
     void testD(){
         System.out.println("Running testD");
     }
