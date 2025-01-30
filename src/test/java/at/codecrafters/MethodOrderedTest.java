5a6fc949b74e941b537ed6082b47d1fcce8ea951
wienm <martin.scheibelreiter@chello.at>
2025-01-30 14:50:26 +0100

    TestInstance.Lifecycle.PER_CLASS

--- src/test/java/at/codecrafters/MethodOrderedTest.java
+++ src/test/java/at/codecrafters/MethodOrderedTest.java
@@ -1,30 +1,40 @@
 package at.codecrafters;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedTest {
+
+    StringBuilder completed = new StringBuilder();
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("Completed: " + completed);
+    }
+
     @Test
     @Order(4)
     void testA(){
         System.out.println("Running testA");
+        completed.append("A");
     }
     @Test
     @Order(2)
     void testB(){
         System.out.println("Running testB");
+        completed.append("B");
     }
     @Test
     @Order(3)
     void testC(){
         System.out.println("Running testC");
+        completed.append("C");
     }
     @Test
     @Order(1)
     void testD(){
         System.out.println("Running testD");
+        completed.append("D");
     }
 }
